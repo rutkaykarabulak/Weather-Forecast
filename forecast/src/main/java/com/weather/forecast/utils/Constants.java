fe15c03fe29fbeb0da6509a944b2bb6fc3734e5c
rutkaykarabulak <rutkaykarabulak@gmail.com>
2023-11-17 22:06:41 +0300

    Remove sensetive data

--- forecast/src/main/java/com/weather/forecast/utils/Constants.java
+++ forecast/src/main/java/com/weather/forecast/utils/Constants.java
@@ -4,7 +4,7 @@ package com.weather.forecast.utils;
 public final class Constants {
     private Constants(){};
 
-    public static final String API_KEY = "67b83ef77c297ef732ee57c131020e71";
+    public static final String API_KEY = "YOURAPIKEY";
     public static final String weatherForecastBaseURL = "https://api.openweathermap.org/data/3.0/onecall?";
     public static final String geoLocationBaseURL = "http://api.openweathermap.org/geo/1.0/direct?";
 }
